cdab24bffc1087267745e2be060d595cabbd063a
Marshall-Bits <kz_@hotmail.es>
2025-08-08 09:33:49 +0200

    format User class

--- src/main/java/dev/marshallBits/estim/models/User.java
+++ src/main/java/dev/marshallBits/estim/models/User.java
@@ -13,17 +13,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class User {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     @Column(nullable = false, unique = true)
     private String username;
-
+    
     @Column(nullable = false, unique = true)
     private String email;
-
+    
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     @Builder.Default
